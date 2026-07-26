58e0cf21f7932e3c445b64b3175868130117ec51
mohamadrezaIzadi <152737951+mmd0211@users.noreply.github.com>
2026-07-26 20:30:32 +0330

    Use separate application id for Sigma Class Planner

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,7 +7,7 @@ android {
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "ir.sigmarobotic.sigmaplanner"
+        applicationId = "ir.sigmarobotic.sigmaclassplanner"
         minSdk = 24
         targetSdk = 35
         versionCode = 1
